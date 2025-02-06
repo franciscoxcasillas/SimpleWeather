3541c138da801742374ea47819c28ae9ba46f3a1
Francisco Casillas <chiccasillas@gmail.com>
2025-02-06 12:05:55 -0600

    Initial Commit

---
+++ SimpleWeather/SimpleWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleWeatherApp.swift
+//  SimpleWeather
+//
+//  Created by Francisco Jean on 06/02/25.
+//
+
+import SwiftUI
+
+@main
+struct SimpleWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
