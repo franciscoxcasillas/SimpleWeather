edcc16816149643ee396b38b6ae4e73b8a4e01c6
Francisco Casillas <chiccasillas@gmail.com>
2025-02-06 13:31:25 -0600

    Import images and set backgrounds. Create basic layout with placeholder text

--- SimpleWeather/ContentView.swift
+++ SimpleWeather/ContentView.swift
@@ -9,13 +9,103 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Image("sunny")
+                .resizable()
+                .scaledToFill()
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                MainView()
+                Spacer()
+                AirQualityView()
+                    .padding(.leading, -70)
+                WeeklyView()
+            }
+            .padding(.top, 45)
+            .padding(.bottom, 10)
+          
         }
-        .padding()
+    }
+}
+
+struct MainView: View {
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text("Mexico City")
+                .font(.system(size: 45, weight: .bold))
+                .padding(.bottom, 0)
+            Text("20°")
+                .font(.system(size: 55))
+        }
+        .padding(.leading, -90)
+        
+    }
+}
+
+struct WeeklyView: View {
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text("Next week")
+                .padding(.leading, 6)
+                .padding(.bottom, 12)
+                .font(.system(size: 30, weight: .bold))
+            HStack {
+                DailyView()
+                DailyView()
+                DailyView()
+                DailyView()
+                DailyView()
+                DailyView()
+        }
+            .padding(.bottom, 25)
+            HStack {
+                DailyView()
+                DailyView()
+                DailyView()
+                DailyView()
+                DailyView()
+                DailyView()
+        }
+        }
+        .padding(.vertical, 45)
+        .padding(.horizontal, 10)
+        .background(
+            LinearGradient(colors: [Color.blue.opacity(0.3), Color.purple.opacity(0.3)], startPoint: .top, endPoint: .bottom)
+        )
+        .cornerRadius(15)
+    }
+}
+
+struct DailyView: View {
+    var body: some View {
+        VStack {
+            Text("Day")
+                .font(.system(size: 20))
+            Image(systemName: "cloud.drizzle.fill")
+                .font(.system(size: 20))
+                .foregroundColor(.black.opacity(0.6))
+                .padding(.vertical,20)
+                .padding(.horizontal, 15)
+            Text("20°")
+                .font(.system(size: 20))
+        }
+    }
+}
+
+struct AirQualityView: View {
+    var body: some View {
+        HStack {
+            Image(systemName: "wind")
+                .foregroundColor(.white)
+            Image(systemName: "thermometer.medium")
+                .foregroundColor(.red)
+            Image(systemName: "humidity.fill")
+                .foregroundColor(.blue)
+            Image(systemName: "carbon.monoxide.cloud.fill")
+                .foregroundColor(.gray)
+        }
+        .font(.system(size: 23))
+        .padding(.bottom, 15)
     }
 }
 
